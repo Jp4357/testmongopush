9c08a2a6a117c84c81628d2f0838251c633f2b35
Jp4357 <jyotiprakash4357@gmail.com>
2022-07-25 12:37:10 +0530

    first code push from pycharm

---
+++ mongotest.py
@@ -0,0 +1,14 @@
+
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://test_jp:vrjp4357@cluster0.32c6x7d.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )

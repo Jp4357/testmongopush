5e59631be94c9e1b1239eccf4b505ba5bc0f06a8
Jp4357 <jyotiprakash4357@gmail.com>
2022-07-25 12:42:02 +0530

    first code push from pycharm 2nd

--- mongotest.py
+++ mongotest.py
@@ -9,6 +9,11 @@ d = {
     "email" : "sudhanshu@ineuron.ai",
     "surname" : "kumar"
 }
+d = {
+    "name":"jp",
+    "email" : "jp@gmail.com",
+    "surname" : "Lenka"
+}
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d )
